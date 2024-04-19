26050dd43b9122ea1daa385f5624dd6fc36d0e57
Robert Feldstein <rhf83@cornell.edu>
2024-04-19 17:22:51 -0400

    small change

--- seesaw/R/temperature_trends.R
+++ seesaw/R/temperature_trends.R
@@ -24,39 +24,30 @@ trend_of_temps <- function(){
 
   for (station in 1:length(stations)) {
     print(station)
+    station_slopes <- numeric(12)
     # Filter the data to include only the station of interest
     for (month in 1:12){
       station_data <- full_table[full_table$DAY == 1
                                  & full_table$MONTH == month
                                  & full_table$WBANNO == stations[station], ]
+      if (nrow(station_data) < 2) {
+        next
+      }
+      lm_fit <- lm(T_DAILY_AVG ~ LST_DATE, data = station_data)
+      slope <- lm_fit$coefficients[2]
 
+      # Convert the slope to units of degrees Celsius per year
+      slope_degrees_per_year <- slope * 365
+      station_slopes[month] <- slope_degrees_per_year
     }
-    # Filter the data to include only the station of interest
-
-    if (nrow(station_data) < 2) {
-      next
-    }
-
-    # Look at the trend of temperatures over time
-    lm_fit <- lm(T_DAILY_AVG ~ LST_DATE, data = station_data)
-
-    # Extract the slope of the linear model
-    slope <- lm_fit$coefficients[2]
-
-    # Convert the slope to units of degrees Celsius per year
-    slope_degrees_per_year <- slope * 365
-    slope_coefs[station] <- slope_degrees_per_year
+    slope_coefs[station, 2:13] <- station_slopes
   }
-
-  # Make a data.frame with station id and slope_coef
-
-  slope_coefs <- data.frame(station_id = stations,
-                            slope_coef = slope_coefs)
-
+  slope_coefs$mean <- rowMeans(slope_coefs[,2:13], na.rm = TRUE)
   return(slope_coefs)
 }
 
 val <- trend_of_temps()
+View(val)
 mean(val$slope_coef, na.rm = TRUE)
 
 
