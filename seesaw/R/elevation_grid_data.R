45f4524385221fd5465b77093255c4c4dea92fb1
Robert Feldstein <rhf83@cornell.edu>
2024-05-05 23:00:46 -0400

    removed a bunch of notes and warnings

---
+++ seesaw/R/elevation_grid_data.R
@@ -0,0 +1,13 @@
+#'
+#' An R dataframe of latitude and longitude over the continental United States
+#'
+#' The dataframe of points is composed for a resolution 50 grid.
+#'
+#' @format A data frame with 3 columns and 1372 rows
+#'
+#' \describe{
+#'  \item{x}{a numeric vector of longitude values}
+#'  \item{y}{a numeric vector of latitude values}
+#'  \item{elevation}{a numeric vector of elevation values}
+#'  }
+"elevation_grid"
