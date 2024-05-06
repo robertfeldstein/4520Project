76e7df062c14470c7b1e71c0e218b7722a84f222
Robert Feldstein <rhf83@cornell.edu>
2024-05-06 14:41:49 -0400

    added note next to devtools

--- seesaw/R/globals.R
+++ seesaw/R/globals.R
@@ -1,1 +1,4 @@
+
+# A variable to set the global variables for the project
+# These variables are used in functions and are the main data
 utils::globalVariables(c("full_table", "shp_file", "station_info"))
